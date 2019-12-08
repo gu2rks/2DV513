05c0eafd6f39a430d9ca779c13cc438b098c32a0
amata a <ch15mt.ag@gmail.com>
2019-12-08 20:13:51 +0100

    add comment and cleaned the script

--- LAB2/src/db.py
+++ LAB2/src/db.py
@@ -8,7 +8,9 @@ import time
 SUBREDDIT = "Subreddit"
 COMMENT = "Comment"
 LINK = "Link"
-def connect ():
+
+
+def connect(): # connect to mysql database
     try:
         mydb = mysql.connector.connect(
             host="localhost",
@@ -21,24 +23,27 @@ def connect ():
         sys.exit("Can't connect to database")
     return mydb
 
-def readFile ():
-    fileHandler = open (sys.argv[3], "r")
+
+def readFile(): # read file
+    fileHandler = open(sys.argv[3], "r")
     data = fileHandler.readlines()
     fileHandler.close()
-    return  data
+    return data
 
-def saveToDatabase (cursor, table, item):
-    if table == SUBREDDIT:
-        mySql_insert_query = 'INSERT INTO Subreddit (id, name) VALUES (%s, %s) '
+
+def saveToDatabase(cursor, table, item): # insert data in mysql table by table
+    if table == SUBREDDIT: # insert in subreddit table
+        mySql_insert_query = 'INSERT INTO Subreddit (id, name) VALUES (%s, %s) ' 
         val = (item["subreddit_id"], item["subreddit"])
         cursor.execute(mySql_insert_query, val)
-    elif table == LINK:
+    elif table == LINK: # insert to Link table
         mySql_insert_query = 'INSERT INTO Link (id) VALUES (%s)'
         val = (item["link_id"],)
         cursor.execute(mySql_insert_query, val)
-    else:
+    else:   # insert to Comment table
         mySql_insert_query = 'INSERT INTO Comment (id, name, author, createdUTC, parentID, body, score) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-        val = (item["id"], item["name"], item["author"], item["created_utc"], item["parent_id"], item["body"], item["score"] )
+        val = (item["id"], item["name"], item["author"], item["created_utc"],
+               item["parent_id"], item["body"], item["score"])
         cursor.execute(mySql_insert_query, val)
 
 # def thread_function(mydb, item):
@@ -49,25 +54,23 @@ def saveToDatabase (cursor, table, item):
 #     saveToDatabase(cursor, LINK, j)
 #     mydb.commit()
 
-
 if len(sys.argv) - 1 <= 2:
     print('ERROR!! python3 db.py [password] [NameDatabase] [FileAbsolutePath]')
 else:
     mydb = connect()
-    data = readFile()
+    data = readFile() 
     # threads = list()
     start_time = time.time()
-    for item in data: # insert data to subreddit table
-
-        ## Threadding
+    for item in data:
+        # Threadding
         # x = threading.Thread(target=thread_function, args=(mydb,item))
         # threads.append(x)
         # x.start()
         # x.join()
         cursor = mydb.cursor()
-        j = json.loads(item)
+        j = json.loads(item) # convert to item to json
         saveToDatabase(cursor, SUBREDDIT, j)
-        saveToDatabase(cursor, COMMENT, j)
         saveToDatabase(cursor, LINK, j)
-        mydb.commit()
+        saveToDatabase(cursor, COMMENT, j)
+        mydb.commit() # commit changes in databes
     print("--- %s seconds ---" % (time.time() - start_time))
