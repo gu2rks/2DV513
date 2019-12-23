3ae3db23abb81128f3db7aec7937aeb0ebd0a0cc
amata a <ch15mt.ag@gmail.com>
2019-12-23 14:33:26 +0100

    Updated view
    + book management
    + invalid input

--- LAB3/src/view.py
+++ LAB3/src/view.py
@@ -1,7 +1,16 @@
-def home(): # main menu/home page
+def homeView(): # main menu/home page
     print('')
     print('Press 1: menage member')
     print('Press 2: manage book')
     print('Press 3: manage loan')
-    return input('#')
-    
+    print('Press 4: exit')
+    return int(input('# '))
+
+def bookView(): # book manu
+    print('Press 1: add book')
+    print('Press 2: edit book')
+    print('Press 3: delete book')
+    return int(input('# '))
+
+def invalidInput():
+    print('[+] ERROR!: Invalid input')
